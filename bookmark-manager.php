caea25d13d1f67721600c38e7d5c3e4c3e6c5f62
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2017-06-20 15:58:21 +0200

    Add PHP Check. Don't activate plugin if PHP <=5.4 is installed

--- bookmark-manager.php
+++ bookmark-manager.php
@@ -29,20 +29,40 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-if(!defined('ABSPATH')) die();
+if ( ! defined( 'ABSPATH' ) ) {
+    die();
+}
 
 require( __DIR__ . '/vendor/autoload.php' );
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-// Initialize plugin - Change to use your own namespace
-new \BookmarkManager\BookmarkManager(array(
-	'data' => get_plugin_data(__FILE__),
-	'path' => realpath(plugin_dir_path(__FILE__)).DIRECTORY_SEPARATOR,
-	'url' => plugin_dir_url(__FILE__),
-	'textdomain' => 'bookmark-manager',
-	'object_cache_group' => 'bookmark_manager_cache',
-	'object_cache_expire' => 72, // In hours
-	'prefix' => 'bookmark_manager_', // Change to your own unique field prefix
-    'version' => '0.1.0', // Current Version
-));
+$updatePhp = new WPUpdatePhp( '5.4.0' );
+$updatePhp->set_plugin_name( 'Bookmark Manager' );
 
+if ( $updatePhp->does_it_meet_required_php_version() ) {
+
+    // Initialize plugin - Change to use your own namespace
+    return new \BookmarkManager\BookmarkManager( [
+        'data'                => get_plugin_data( __FILE__ ),
+        'path'                => realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR,
+        'url'                 => plugin_dir_url( __FILE__ ),
+        'textdomain'          => 'bookmark-manager',
+        'object_cache_group'  => 'bookmark_manager_cache',
+        'object_cache_expire' => 72, // In hours
+        'prefix'              => 'bookmark_manager_', // Change to your own unique field prefix
+        'version'             => '0.1.0', // Current Version
+    ] );
+
+} else {
+    add_action( 'admin_init', function () {
+        // Deactivate Plugin
+        deactivate_plugins( plugin_basename( __FILE__ ) );
+    } );
+
+    add_action( 'admin_notices', function () {
+        // Remove 'Plugin activated' message
+        if ( isset( $_GET[ 'activate' ] ) ) {
+            unset( $_GET[ 'activate' ] );
+        }
+    } );
+}

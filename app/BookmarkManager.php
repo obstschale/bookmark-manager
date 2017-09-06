bead49e07121406e4cc1ea4b01541aa4808d43f8
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2017-09-06 12:28:12 +0200

    🔨🔥 Remove methods and implement service register

--- app/BookmarkManager.php
+++ app/BookmarkManager.php
@@ -2,30 +2,30 @@
 
 namespace BookmarkManager;
 
+use BookmarkManager\Metaboxes;
+use BookmarkManager\PostTypes;
+use BookmarkManager\Taxonomies;
 use BookmarkManager\Admin\AdminFooter;
 use BookmarkManager\Settings\Settings;
 use BookmarkManager\Widgets\WidgetLoader;
-use BookmarkManager\PostTypes\BookmarksPostType;
+use BookmarkManager\Provider\ServiceProvider;
 use BookmarkManager\REST\REST_Bookmarks_Controller;
 
-class BookmarkManager
+class BookmarkManager implements Registerable
 {
 
     public static $settings;
-
-    public static $version;
 
     public static $prefix;
 
     protected static $registry;
 
+    protected $enqueue_scripts;
+
 
     function __construct()
     {
         $_settings = [
-            'data'                => get_plugin_data( __FILE__ ),
-            'path'                => realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR,
-            'url'                 => plugin_dir_url( __FILE__ ),
             'object_cache_group'  => 'bookmark_manager_cache',
             'object_cache_expire' => 72, // In hours
             'prefix'              => 'bookmark_manager_', // Change to your own unique field prefix
@@ -34,17 +34,11 @@ class BookmarkManager
         // Set text domain and option prefix
         self::$settings = $_settings;
         self::$prefix   = $_settings[ 'prefix' ];
-        self::$version  = $_settings[ 'data' ][ 'Version' ];
 
-        // Enqueue scripts
-        new EnqueueScripts();
-
+        //$this->enqueue_scripts = $enqueue_scripts;
 
         // Add admin settings page(s)
-        new Settings();
-
-        // Create Custom Post Type(s)
-        new BookmarksPostType();
+        //new Settings();
 
         // Create custom widgets
         //new WidgetLoader();
@@ -83,7 +77,7 @@ class BookmarkManager
             return $value;
         }
 
-        return self::$registry['data'][ $key ] ?? false;
+        return self::$registry[ 'data' ][ $key ] ?? false;
     }
 
 
@@ -123,117 +117,50 @@ class BookmarkManager
 
 
     /**
-     * Returns true if request is via Ajax.
+     * Register the plugin with the WordPress system.
      *
-     * @return bool
+     * @since 0.2.0
+     *
+     * @return void
      */
-    public function is_ajax()
+    public function register()
     {
-        return defined( 'DOING_AJAX' ) && DOING_AJAX;
+        add_action( 'plugins_loaded', [ $this, 'register_services' ] );
     }
 
 
     /**
-     * Returns script ?ver= version based on environment (WP_ENV)
+     * Register the individual services of this plugin.
      *
-     * If WP_ENV is not defined or equals anything other than 'development' or 'staging'
-     * returns $script_version (if defined) else plugin verson. If WP_ENV is defined
-     * as 'development' or 'staging', returns string representing file last modification
-     * date (to discourage browser during development).
-     *
-     * @param string $script         The filesystem path (relative to the script location of
-     *                               calling script) to return the version for.
-     * @param string $script_version (optional) The version that will be returned if
-     *                               WP_ENV is defined as anything other than 'development' or 'staging'.
-     *
-     * @return string
+     * @since 0.2.0
      */
-    public function get_script_version( $script, $return_minified = false, $script_version = null )
+    public function register_services()
     {
-        $version = $script_version ?: self::$settings[ 'data' ][ 'Version' ];
-        if ( $this->is_production() ) {
-            return $version;
-        }
+        $services = $this->get_services();
 
-        $script = $this->get_script_path( $script, $return_minified );
-        if ( file_exists( $script ) ) {
-            $version = date( "ymd-Gis", filemtime( $script ) );
-        }
-
-        return $version;
-    }
-
-
-    /**
-     * Returns script path or URL, either regular or minified (if exists).
-     *
-     * If in production mode or if @param $force_minify == true, inserts '.min' to the filename
-     *                                (if exists), else return script name without (example: style.css vs
-     *                                style.min.css).
-     *
-     * @param string $script          The relative (to the plugin folder) path to the script.
-     * @param bool   $return_minified If true and is_production() === true then will prefix the
-     *                                extension with .min. NB! Due to performance reasons, I did not include logic to
-     *                                check to see if the script_name.min.ext exists, so use only when you know it
-     *                                exists.
-     * @param bool   $return_url      If true, returns full-qualified URL rather than filesystem path.
-     *
-     * @return string The URL or path to minified or regular $script.
-     */
-    public function get_script_path( $script, $return_minified = false, $return_url = false )
-    {
-        $script = trim( $script, '/' );
-        if ( $return_minified && strpos( $script, '.' ) && $this->is_production() ) {
-            $script_parts     = explode( '.', $script );
-            $script_extension = end( $script_parts );
-            array_pop( $script_parts );
-            $script = implode( '.', $script_parts ) . '.min.' . $script_extension;
-        }
-
-        return self::$settings[ $return_url ? 'url' : 'path' ] . $script;
-    }
-
-
-    /**
-     * Returns absolute URL of $script.
-     *
-     * @param string $script The relative (to the plugin folder) path to the script.
-     * @param        bool
-     */
-    public function get_script_url( $script, $return_minified = false )
-    {
-        return $this->get_script_path( $script, $return_minified, true );
-    }
-
-
-    /**
-     * Merges two array, eliminating duplicates
-     *
-     * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
-     * Matching keys' values in the second array overwrite those in the first array, as is the
-     * case with array_merge().
-     *
-     * @param array $array1
-     * @param array $array2
-     *
-     * @return array
-     * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
-     * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
-     */
-    private function array_merge_recursive_distinct( array &$array1, array &$array2 )
-    {
-        // Credit: http://php.net/manual/en/function.array-merge-recursive.php#92195
-        $merged = $array1;
-
-        foreach ( $array2 as $key => &$value ) {
-            if ( is_array( $value ) && isset ( $merged [ $key ] ) && is_array( $merged [ $key ] ) ) {
-                $merged [ $key ] = self::array_merge_recursive_distinct( $merged [ $key ], $value );
-            } else {
-                $merged [ $key ] = $value;
+        foreach ( $services as $class ) {
+            $service = new $class();
+            if ( $service instanceof Service ) {
+                $service->register();
             }
         }
-
-        return $merged;
     }
 
+
+    /**
+     * Get services to register for this plugin.
+     *
+     * @since 0.2.0
+     *
+     * @return array Array of services to register.
+     */
+    private function get_services() : array
+    {
+        return [
+            Taxonomies\BookmarkTags::class,
+            Metaboxes\BookmarkLink::class,
+            PostTypes\Bookmarks::class,
+            Settings::class,
+        ];
+    }
 }

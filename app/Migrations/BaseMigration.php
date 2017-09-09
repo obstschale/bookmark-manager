ff15c8b41c5af3bc9c830dcfb0923587ce93a863
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2017-09-09 21:41:48 +0200

    🗃  Add DB_Version_1 migration #26

--- app/Migrations/BaseMigration.php
+++ app/Migrations/BaseMigration.php
@@ -2,13 +2,27 @@
 
 namespace BookmarkManager\Migrations;
 
+/**
+ * BaseMigration or base chain element.
+ *
+ * Each migration extends BaseMigration to be proper chain element.
+ *
+ * DB version means in this context, the Bookmark Manager DB version, not WordPress'.
+ *
+ * @since   0.2.0
+ *
+ * @package BookmarkManager\Migrations
+ */
 abstract class BaseMigration
 {
 
+    /**
+     * DB version option name.
+     */
     const OPTION_NAME = 'bookmark_manager_db_version';
 
     /**
-     * @var BaseMigration
+     * @var BaseMigration Successor of this element.
      */
     protected $next;
 
@@ -18,18 +32,54 @@ abstract class BaseMigration
     protected $current_version;
 
 
+    /**
+     * BaseMigration constructor.
+     *
+     * Set next chain element (successor). This migration will be called after
+     * this migration has finished. If $next is NULL the chain ends and this is
+     * the last migration.
+     *
+     * @param BaseMigration|null $next Successor in chain.
+     */
     public function __construct( BaseMigration $next = null )
     {
         $this->next = $next;
     }
 
 
+    /**
+     * Migrate method does some checks and calls the update method if necessary.
+     *
+     * Firstly, check if this migration is newer than the current DB version. If so
+     * the update procedure is called and the result is checked for \WP_Error.
+     *
+     * The next chain element (next migration) is called as long as no error is found.
+     *
+     * @param int $version Current DB version.
+     *
+     * @since 0.2.0
+     *
+     * @return bool Return false if an error is found.
+     */
     final public function migrate( int $version )
     {
         $this->set_current_version( $version );
 
         if ( $this->is_new() ) {
-            $this->update_database();
+            $response = $this->update_database();
+
+            if ( is_wp_error( $response ) ) {
+                /** @var $response \WP_Error */
+                add_action( 'admin_notices', function () use ( $response ) {
+                    $class   = 'notice notice-error';
+                    $message = __( 'Irks! An migration error has occurred.', 'bookmark-manager' );
+
+                    printf( '<div class="%1$s"><p><strong>%2$s</strong></p><p>%3$s</p></div>',
+                        esc_attr( $class ), esc_html( $message ), $response->get_error_message() );
+                } );
+
+                return false;
+            }
         }
 
         if ( ! is_null( $this->next ) ) {
@@ -38,30 +88,76 @@ abstract class BaseMigration
     }
 
 
+    /**
+     * Update procedure, which runs one or more SQL queries and updates the database.
+     *
+     * The method should either return the result of the query or return an \WP_Error instance
+     * with a proper error code added to it.
+     *
+     * Don't for get to call $this->bump_db_version() afterwards to update the option.
+     *
+     * @since 0.2.0
+     *
+     * @return int|\WP_Error SQL result or \WP_Error instance if an error occurred.
+     */
     abstract public function update_database();
 
 
+    /**
+     * Update DB version number.
+     *
+     * @param int $version New version.
+     *
+     * @return bool Result of update_option() call.
+     */
     public function bump_db_version( int $version ) : bool
     {
         return update_option( self::OPTION_NAME, $version );
     }
 
 
+    /**
+     * Check if this migration is newer than the current DB version.
+     *
+     * @since 0.2.0
+     *
+     * @return bool Return true if migration needs to run, otherwise false.
+     */
     public function is_new()
     {
         return $this->get_current_version() < $this->get_version();
     }
 
 
+    /**
+     * Get database version number, which this migration sets.
+     *
+     * @since 0.2.0
+     *
+     * @return int
+     */
     abstract public function get_version() : int;
 
 
+    /**
+     * Set current version, which is the current version in DB.
+     * @since 0.2.0
+     *
+     * @param int $version DB Version
+     */
     public function set_current_version( int $version ) : void
     {
         $this->current_version = $version;
     }
 
 
+    /**
+     * Get current version, which is the current version in DB.
+     *
+     * @since 0.2.0
+     *
+     * @return int DB Version
+     */
     public function get_current_version() : int
     {
         return $this->current_version;
--- app/Migrations/MigrationService.php
+++ app/Migrations/MigrationService.php
@@ -4,6 +4,21 @@ namespace BookmarkManager\Migrations;
 
 use BookmarkManager\Service;
 
+/**
+ * MigrationService builds a chain of migrations and runs them.
+ *
+ * The migration service run before other bookmark manager features are registered.
+ * It builds a 'Chain of Responsibility' of migrations and runs migrations if a DB
+ * update is necessary. That means, if the 'bookmark_manager_db_version' option is
+ * behind the last migration version.
+ *
+ * Every migrations is build, but decides itself if the update procedure should run.
+ * So all migrations in the chain are called but only needed updates are performed.
+ *
+ * @since   0.2.0
+ *
+ * @package BookmarkManager\Migrations
+ */
 class MigrationService implements Service
 {
 
@@ -18,6 +33,12 @@ class MigrationService implements Service
     protected $current_version;
 
 
+    /**
+     * MigrationService constructor.
+     *
+     * Call get_chain() to populate the $chain property and fetch current
+     * DB version to check later if migrations need to run.
+     */
     public function __construct()
     {
         $this->get_chain();
@@ -28,27 +49,50 @@ class MigrationService implements Service
     /**
      * Register the current Registerable.
      *
-     * @since 0.1.0
+     * @since 0.2.0
      *
      * @return void
      */
-    public function register()
+    public function register() : void
     {
         add_action( 'init', [ $this, 'migrate' ], 5 );
     }
 
 
+    /**
+     * Migration method kicks off the chain of migrations.
+     *
+     * Firstly, check if any migration is needed. If not no chain is build.
+     * Because the build of process creates new object, which we can avoid.
+     *
+     * If a migration is needed the chain is built up and the first migration
+     * is called with the current DB version. If the migration is finished it
+     * will call the next migration until no migration is left.
+     *
+     * @since 0.2.0
+     */
     public function migrate() : void
     {
         if ( $this->migration_needed() ) {
             $migration = $this->build_chain();
 
-            $version   = $this->get_current_version();
+            $version = $this->get_current_version();
             $migration->migrate( $version );
         }
     }
 
 
+    /**
+     * Build the chain of migrations and return the first element of that chain.
+     *
+     * A new object of each migration (element) is instantiated and passed as successor
+     * to the previous migration. So the chain is built backwards to start with the first
+     * and end with the last migration.
+     *
+     * @since 0.2.0
+     *
+     * @return BaseMigration First migration, which is the beginning of the chain.
+     */
     public function build_chain() : BaseMigration
     {
         $chain     = $this->get_chain();
@@ -62,6 +106,13 @@ class MigrationService implements Service
     }
 
 
+    /**
+     * Check the DB version to know if a new migration is available.
+     *
+     * @since 0.2.0
+     *
+     * @return bool Whether a migration needs to run.
+     */
     public function migration_needed() : bool
     {
         $chain            = $this->get_chain();
@@ -71,6 +122,17 @@ class MigrationService implements Service
     }
 
 
+    /**
+     * Return the chain array, which holds all migration class references.
+     *
+     * The migration classes must be sorted from oldest to newest. It is the best
+     * to simply name migrations according their DB version. So the sorting is more
+     * obvious.
+     *
+     * @since 0.2.0
+     *
+     * @return array Array with all migration classes.
+     */
     protected function get_chain() : array
     {
         /**
@@ -85,6 +147,16 @@ class MigrationService implements Service
     }
 
 
+    /**
+     * Get the current DB version, which is saved in DB.
+     *
+     * If no DB version is yet set, the default value of 0 is returned. That also means
+     * the first migration will run and set the version to 1.
+     *
+     * @since 0.2.0
+     *
+     * @return int DB Version saved in DB
+     */
     public function get_current_version() : int
     {
         if ( empty( $this->current_version ) ) {
